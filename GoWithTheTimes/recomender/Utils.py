e364ec59d826e15494ae2486d85fad7a874b56cd
Wei-Jianan <weij@tcd.ie>
2018-03-20 00:03:27 +0000

    user model module framework done

--- GoWithTheTimes/recomender/Utils.py
+++ GoWithTheTimes/recomender/Utils.py
@@ -1,3 +1,8 @@
 
-def find_standard_error(vector_l, vector_r):
+def find_standard_error(vector_list):
+    # TODO
     return 0.1
+
+def find_consine(vector_l, vector_r):
+    # TODO
+    return sum(vector_l)
